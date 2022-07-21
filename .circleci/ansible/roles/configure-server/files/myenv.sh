499ac86e5f6a08610721ac5fb1d638033bc5d755
Gerald.okeke <okekegerald@gmail.com>
2022-07-21 16:24:54 +0100

    run migrations

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -5,7 +5,7 @@ export TYPEORM_MIGRATIONS=./src/migrations/*.ts
 
 # Use these values for the local PG database from the Docker Compose file
 export TYPEORM_HOST=udaproj3new.cgw52ul9trwe.us-east-1.rds.amazonaws.com
-export TYPEORM_PORT=5532
+export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
 export TYPEORM_PASSWORD=Password10$
 export TYPEORM_DATABASE=udadb
